a1184a8c0fc4bcdf37ab8a48247238f391d3657c
Christine Awesome <christine@neato.co.nz>
2012-06-09 11:12:27 -0400

    Adds a pile of data about bus stops;  and loads it as JSON.

--- magic.js
+++ magic.js
@@ -1,13 +1,23 @@
 var yellowApiKey = 'exbpfptg75sv33yfyyjhtsb4'; // Sandbox key.  Not useful.
+var stops = null;
+var map;
+
+function getStops() {
+	jQuery.get('data/stops.json', function (json) {
+		stops = json;
+	});
+}
 
 function init() {
 	// replace "toner" here with "terrain" or "watercolor"
 	var layer = new L.StamenTileLayer("toner");
-	var map = new L.Map("map", {
+	map = new L.Map("map", {
 	    center: new L.LatLng(45.5081, -73.5550),
 	    zoom: 12
 	});
 	map.addLayer(layer);
+	
+	getStops();
 }
 
 jQuery(document).ready(init);
