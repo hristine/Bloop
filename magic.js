e58c308e5ce850163e4e23fa4a7abfed935ff08b
Christine Awesome <christine@neato.co.nz>
2012-06-09 11:38:10 -0400

    Now with Local!  Search for locations on the map.

--- magic.js
+++ magic.js
@@ -2,6 +2,24 @@ var yellowApiKey = 'exbpfptg75sv33yfyyjhtsb4'; // Sandbox key.  Not useful.
 var stops = null;
 var map;
 
+function changeNeighbourhood(e) {
+	e.preventDefault();
+	
+	jQuery.get('http://nominatim.openstreetmap.org/search?json_callback=?', 
+		{
+			'q': jQuery('#location').val() + ', Montreal',
+			'format': 'json'
+		}, 
+		function (data) {
+			// Let's just assume the geocoder is AMAZING.
+			if (data.length > 0) {
+				map.panTo(new L.LatLng(data[0].lat, data[0].lon));
+			} else {
+				alert('Sad geocoder is sad.  Blurp.');
+			}
+		}, 'jsonp');
+}
+
 function getStops() {
 	jQuery.get('data/stops.json', function (json) {
 		stops = json;
@@ -13,11 +31,13 @@ function init() {
 	var layer = new L.StamenTileLayer("toner");
 	map = new L.Map("map", {
 	    center: new L.LatLng(45.5081, -73.5550),
-	    zoom: 12
+	    zoom: 15
 	});
 	map.addLayer(layer);
 	
 	getStops();
+	
+	jQuery('#changeLocation').click(changeNeighbourhood);
 }
 
 jQuery(document).ready(init);
