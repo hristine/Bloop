d9a5faae2af0003eae8593c5b0c0c167cc1f5d6d
Christine Awesome <christine@neato.co.nz>
2012-06-09 10:53:46 -0400

    Show a map.  Because there's going to be a map.

--- magic.js
+++ magic.js
@@ -1,1 +1,13 @@
 var yellowApiKey = 'exbpfptg75sv33yfyyjhtsb4'; // Sandbox key.  Not useful.
+
+function init() {
+	// replace "toner" here with "terrain" or "watercolor"
+	var layer = new L.StamenTileLayer("toner");
+	var map = new L.Map("map", {
+	    center: new L.LatLng(45.5081, -73.5550),
+	    zoom: 12
+	});
+	map.addLayer(layer);
+}
+
+jQuery(document).ready(init);
